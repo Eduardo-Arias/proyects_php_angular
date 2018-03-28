aed64c0bb1bd3d688b258888062f481df8c6d3dd
Eduardo-Arias <delgado.jorge.eduardo95@gmail.com>
2018-03-27 18:51:13 -0600

    Add files via upload

---
+++ Proyect_angular_php/app/app.js
@@ -0,0 +1,14 @@
+// Se defino el modulo para la app
+var app = angular.modulo('app',[]);
+
+app.controller('appController', [ function($scope){
+	$scope.function listarContenido(argument) {
+		// body...
+	}
+	$scope.function buscarContenido(argument) {
+		// body...
+	}
+	$scope.function iniciarLogin(argument) {
+		// body...
+	}
+}]);
---
+++ Proyect_angular_php/app/usuarioApp.js
@@ -0,0 +1,24 @@
+var usuarioApp = angular./**
+*  Module
+*
+* Description
+*/
+angular.module('usuarioApp', []);
+
+usuarioApp.controller('usuarioControler', [function($scope,$http){
+	$scope.function registrarUsuario(argument) {
+		// body...
+	}
+	$scope.function buscarUsuario(argument) {
+		// body...
+	}
+	$scope.function editarUsuario(argument) {
+		// body...
+	}
+	$scope.function listarUsuarios(argument) {
+		// body...
+	}
+	$scope.function eliminarUsuario(argument) {
+		// body...
+	}
+}])
